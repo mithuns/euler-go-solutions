c4d2afd248cd9893987fe39118dba07d252fb4ad
Mithun <m.singh2005@gmail.com>
2018-01-31 21:15:52 -0600

    Create Euler_2.go

---
+++ Euler_2.go
@@ -0,0 +1,39 @@
+package main
+import "fmt"
+
+func main() {
+ //Enter your code here. Read input from STDIN. Print output to STDOUT
+    var numCases int
+    fmt.Scanf("%d", &numCases)
+    
+    for i:=1;i<=numCases;i++{
+        var N int
+        fmt.Scanf("%d",&N)
+        EvenSumFibo(N)
+    }    
+}
+
+func EvenSumFibo(N int){
+    if N==1 || N==1{
+        fmt.Println(0)
+        return
+    }
+    if N==2{
+        fmt.Println(2)
+        return
+    }
+    var a,b,c=1,2,0
+    var sum =b
+    for i:=3;i<=N;i++{
+        c = a+b
+        if(c>=N){
+            break
+        }
+        if(c%2==0){
+            sum = sum+c
+        }
+        a = b
+        b = c
+    }
+    fmt.Println(sum)
+}
